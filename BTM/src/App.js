d966ec2d3ea018c04a300de088f26fb4bdf0e035
Nyshanbek Tomiris <t_nyshanbek@kbtu.kz>
2024-06-04 12:06:05 +0500

    1

---
+++ BTM/src/App.js
@@ -0,0 +1,17 @@
+function Mybutton() {
+  return (
+      <button>
+        Button
+      </button>
+  );
+}
+
+export default function MyApp() {
+  return (
+      <div>
+        <h1>Hello, Baitau!</h1>
+        <MyButton />
+      </div>
+  )
+}
+
