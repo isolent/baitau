b0f52e2c1a19ed8c0729fa67cfd6c200050e693d
Nyshanbek Tomiris <t_nyshanbek@kbtu.kz>
2024-06-11 15:28:32 +0500

    aboutUs part

--- BTM/src/App.js
+++ BTM/src/App.js
@@ -2,23 +2,27 @@ import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
 import MiningStages from './components/MiningStages';
+import About from './components/About';
 
 export default function MyApp() {
   return (
     <div id="app">
     <Router>
       <Header />
-    <Routes>
-          <Route path="/" element={<Home />} />
-    </Routes>
+      <Home />
+      <div className="Mining stages">
+        <header className="App-header">
+        </header>
+        <main>
+          <MiningStages />
+        </main>
+      </div>
+      <About />
+
+
+
     </Router>
-    <div className="Mining stages">
-      <header className="App-header">
-      </header>
-      <main>
-        <MiningStages />
-      </main>
-    </div>
+
   </div>
   )
 }
