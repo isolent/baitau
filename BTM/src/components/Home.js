57fdd0da8d3d5cf85c93b209212f2590bbe7177b
isolent <a_kakharmanova@kbtu.kz>
2024-06-11 14:24:48 +0500

    fix

--- BTM/src/components/Home.js
+++ BTM/src/components/Home.js
@@ -6,11 +6,9 @@ function Home() {
     <div className="image-container">
      <img src="/images/photo.png" alt="Sample" className="overlay-image" />
       <div className="overlay-text">
-         <p className='big-text'>ЛИДЕРЫ В ДОБЫЧЕ <br/>
-            И ПРОИЗВОДСТВЕ <br/>
-            ЦВЕТНЫХ <br/> МЕТАЛЛОВ</p>
+         <p className='big-text'>ЛИДЕРЫ В ДОБЫЧЕ И ПРОИЗВОДСТВЕ ЦВЕТНЫХ МЕТАЛЛОВ</p>
             <p className='line'>____________________________________________________</p>
-            <p className='welcome-text'> Добро пожаловать в мир качества и <br/> надежности!
+            <p className='welcome-text'> Добро пожаловать в мир качества и надежности!
           </p>
             </div>
     </div>
--- BTM/src/components/StageCard.js
+++ BTM/src/components/StageCard.js
@@ -4,11 +4,13 @@ import './StageCard.css';
 const StageCard = ({ title, image, description }) => {
   return (
     <div className="stage-card">
-      <div className="stage-card__image" style={{ backgroundImage: `url(${image})` }}></div>
-      <h2 className="stage-card__title">{title}</h2>
+      <div className="stage-card__image" style={{ backgroundImage: `url(${image})` }}>
+        <h2 className="stage-card__title">{title}</h2>
+      </div>
       <p className="stage-card__description">{description}</p>
     </div>
   );
 };
+
 
 export default StageCard;
