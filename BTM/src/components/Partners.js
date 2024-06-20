d429ccd65142819a68f27fd1a37f4435c0b564ba
isolent <a_kakharmanova@kbtu.kz>
2024-06-20 10:45:59 +0500

    slider added to partners

--- BTM/src/components/Partners.js
+++ BTM/src/components/Partners.js
@@ -1,24 +1,36 @@
-import './Partners.css'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import './Partners.css';
 import Slider from "react-slick";
+
+const settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 3,
+  slidesToScroll: 3
+};
+
 function Partners() {
-    var settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToScroll: 4,
-        slidesToShow: 4
-      };
-    return (
-        <div className="partners-page">
-            <h1 className="partners-text"> НАМ ДОВЕРЯЮТ</h1>
-                <div className="partners-img">
-                <Slider {...settings}>
-                            <img src="/images/partner1.png" alt="Изображение" />
-                            <img src="/images/partner2.png" alt="Изображение" />
-                            <img src="/images/partner3.png" alt="Изображение" />
-                </div>
-        </div>
-    );
+  return (
+    <div className="partners-page">
+      <h1 className="partners-text"> НАМ ДОВЕРЯЮТ</h1>
+      <div className="partners-img">
+        <Slider {...settings}>
+          <div><img src="/images/partner1.png" alt="Изображение" /></div>
+          <div><img src="/images/partner2.png" alt="Изображение" /></div>
+          <div><img src="/images/partner3.png" alt="Изображение" /></div>
+          <div><img src="/images/partner2.png" alt="Изображение" /></div>
+          <div><img src="/images/partner1.png" alt="Изображение" /></div>
+          <div><img src="/images/partner3.png" alt="Изображение" /></div>
+          <div><img src="/images/partner2.png" alt="Изображение" /></div>
+          <div><img src="/images/partner3.png" alt="Изображение" /></div>
+          <div><img src="/images/partner1.png" alt="Изображение" /></div>
+
+        </Slider>
+      </div>
+    </div>
+  );
 }
 
 export default Partners;
